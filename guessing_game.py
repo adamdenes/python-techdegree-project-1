dff78d4265248f2d05b4741afebbead2f35f8f13
Adam Denes <adam.denes@icloud.com>
2020-03-10 18:50:27 +0100

    Adding highscore display to every iteration, docstrings

--- guessing_game.py
+++ guessing_game.py
@@ -7,48 +7,65 @@ Project 1 - Number Guessing Game
 import random
 
 
-# Store every score from every game
 scores = []
 
-# Return a random number between 1 and 10
 def generate_number():
+    """ Generate a random number between 1 and 10 """
     return random.randint(1, 10)
 
 def get_highscore(attempts_list, counter):
+    """ 
+    This function takes the list of attempts 
+    and the current state of the guess counter.
+    It will return only the lowes amount of attempts.
+    """
     highscore = 0
-    # Loop over the stored scores
     for attempt in attempts_list:
-        # Check if the list is empty or not
-        if len(attempts_list) < 1:
-            return '**no highscore yet**'
-        # If the score is less\equal to the counter save it as highscore
-        elif attempt <= counter:
+        if attempt <= counter:
             highscore = attempt
+        else:
+            highscore = counter
 
     return highscore
 
-def start_game():
-    # Create a counter to store how many turns are required find the number
-    guess_counter = 0
-    # Great the User
-    print('-' * 38 + '\n Welcome to the Number Guessing Game!\n' + '-' * 38 + '\n')
-    print('Current HIGHSCORE: {}'.format(get_highscore(scores, guess_counter)))
 
-    # Use generate_number to generate a number
+def start_game():
+    """
+    This program will welcome the User upon starting the game.
+    It will utilize two different functions, one for random number
+    generation, and one for calculating the highscore.
+
+    The input data will be validated against type and value errors
+    throughout the game, and also handeld accordingly.
+
+    The user will be prompted for input as long as he is not guessing
+    the randomly generated number. The program will keep count of the
+    attempts, and cleares this value if the user decides to play again.
+    In that case only the best attempt is shown.
+
+    If the player quits the game, a farewell messages is printed out.
+    If the player does not want to finish the game, the program can be
+    stopped by pressing CTRL + C. 
+    """
+    guess_counter = 0
+    print('-' * 38 + '\n Welcome to the Number Guessing Game!\n' + '-' * 38 + '\n')
+
+    if guess_counter == 0:
+        print('Current HIGHSCORE: You need to play first!')
     random_solution = generate_number()
 
-    # Try block for potential user inerrupt
     try:
-
-        while True:
+        while random_solution:
             try:
                 guess = input('Pick a number between 1 and 10: ')
                 guess = int(guess)
-                
                 guess_counter += 1
+                #print(f'guesses :: #{guess_counter}')      # to debug guesses
+                #print(f'list-length :: [{len(scores)}]')   # to debug list
                 
                 if guess < 1 or guess > 10:
                     print('{} is out of range [1-10]. Try again...'.format(guess))
+                    guess_counter -= 1
                     continue
                 elif guess < random_solution:
                     print('It is higher!')
@@ -58,6 +75,10 @@ def start_game():
                     continue
                 else:
                     print('\nYou got it! It took you {} tries.'.format(guess_counter))
+                    if len(scores) < 1:
+                        print('Best attempt sofar is {}'.format(guess_counter))
+                    else:
+                        print('Best attempt sofar is {}'.format(get_highscore(scores, guess_counter)))
             except ValueError:
                 print('Only use numbers instead of "{}"!'.format(guess))
                 continue
@@ -67,12 +88,8 @@ def start_game():
                 new_game = input('Would you like to play again? [y]es/[n]o: ')
 
                 if new_game.lower() == 'y':
-                    # Add the number of guesses to the score list
                     scores.append(guess_counter)
-                    # Call the function to display the all time highscore
                     print('\n\nThe HIGHSCORE is {}'.format(get_highscore(scores, guess_counter)))
-
-                    # Reset the counter
                     guess_counter = 0
                     random_solution = generate_number()
                     continue
