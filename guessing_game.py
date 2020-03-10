fed1bb7de6aed06bb2479c15c641f6fd5be71776
Adam Denes <adam.denes@icloud.com>
2020-03-10 15:24:21 +0100

    Fixing highscore feature, cleaning up code

--- guessing_game.py
+++ guessing_game.py
@@ -14,15 +14,28 @@ scores = []
 def generate_number():
     return random.randint(1, 10)
 
+def get_highscore(attempts_list, counter):
+    highscore = 0
+    # Loop over the stored scores
+    for attempt in attempts_list:
+        # Check if the list is empty or not
+        if len(attempts_list) < 1:
+            return '**no highscore yet**'
+        # If the score is less\equal to the counter save it as highscore
+        elif attempt <= counter:
+            highscore = attempt
+
+    return highscore
 
 def start_game():
+    # Create a counter to store how many turns are required find the number
+    guess_counter = 0
     # Great the User
     print('-' * 38 + '\n Welcome to the Number Guessing Game!\n' + '-' * 38 + '\n')
+    print('Current HIGHSCORE: {}'.format(get_highscore(scores, guess_counter)))
 
     # Use generate_number to generate a number
     random_solution = generate_number()
-    # Create a counter to store how many turns are required find the number
-    guess_counter = 0
 
     # Try block for potential user inerrupt
     try:
@@ -54,23 +67,17 @@ def start_game():
                 new_game = input('Would you like to play again? [y]es/[n]o: ')
 
                 if new_game.lower() == 'y':
+                    # Add the number of guesses to the score list
                     scores.append(guess_counter)
+                    # Call the function to display the all time highscore
+                    print('\n\nThe HIGHSCORE is {}'.format(get_highscore(scores, guess_counter)))
 
-                    # Loop over the stored scores
-                    for score in scores:
-                        # If the score is less\equal to the counter
-                        # save it as highscore
-                        if score <= guess_counter:
-                            guess_counter = score
-                            
-                    print('\n\nThe HIGHSCORE is {}'.format(guess_counter))
-    
                     # Reset the counter
                     guess_counter = 0
                     random_solution = generate_number()
                     continue
                 elif new_game.lower() == 'n':
-                    print('Closing game, see you next time!')
+                    print('\nClosing game, see you next time!')
                     break
                 else:
                     raise ValueError('Please type "y" or "n" next time. Exiting...')
